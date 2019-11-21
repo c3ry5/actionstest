da59a60739a84f65de4e16d784541338fc4e4949
Cerys Williams <cwilliams1@penguinrandomhouse.co.uk>
2019-11-21 11:26:15 +0000

    security breaks

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ app.get('/', function (req, res) {
   res.status(200).send('ok');
 });
 
-app.post('/login', function (req, res) {
-  var user = req.body.user;
-  var pass = req.body.pass;
-  var sql  = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'";
-  res.status(200).send(sql);
-});
-
 var server = app.listen(3000, function () {
   var port = server.address().port;
   
