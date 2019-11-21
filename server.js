043f2348672aca17ef83263b235ce5f7e365ddf7
Cerys Williams <cwilliams1@penguinrandomhouse.co.uk>
2019-11-21 11:16:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
 var password = "123qweasd";
+
 app.get('/', function (req, res) {
   res.status(200).send('ok');
 });
+
+app.post('/login', function (req, res) {
+  var user = req.body.user;
+  var pass = req.body.pass;
+  var sql  = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'";
+  return sql;
+});
+
 var server = app.listen(3000, function () {
   var port = server.address().port;
+  
   console.log('Example app listening at port %s', port);
 });
+
+
 module.exports = server;
